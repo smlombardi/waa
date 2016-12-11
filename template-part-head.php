b9db47c15a6327be10c48960d15a9c10b687c9d0
Steve Lombardi <smlombardi@gmail.com>
2016-12-11 17:13:58 -0500

    header finished

--- template-part-head.php
+++ template-part-head.php
@@ -4,18 +4,20 @@
 
 <?php if ($dm_settings['show_header'] != 0) : ?>
 
-    <div class="row dmbs-header">
+    <div class="waa-header">
 
-        <div class="col-md-9">
+        <div class="header-logo">
             <img class="img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/images/waa-logo.png">
         </div>
-        <div class="col-md-3">
+        <div class="header-right">
             <?php get_search_form(); ?>
-            <ul class="list-inline">
-                <li>f</li>
-                <li>b</li>
-                <li>c</li>
-                <li>s</li>
+            <label>Follow Us</label>
+            <ul class="social-links">
+                <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-pinterest.png"></li>
+                <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-tumblr.png"></li>
+                <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-linkedin.png"></li>
+                <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/social-facebook.png"></li>
+               
             </ul>
         </div>
 
