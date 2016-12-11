af39978675216e9a8a49c58609b02bcdcf380da4
Steve Lombardi <smlombardi@gmail.com>
2016-12-11 10:30:34 -0500

    started header

---
+++ page-no_title.php
@@ -0,0 +1,37 @@
+<?php 
+/* template name: No Title */
+
+get_header(); ?>
+
+<?php get_template_part('template-part', 'head'); ?>
+
+<?php get_template_part('template-part', 'topnav'); ?>
+
+<!-- start content container -->
+<div class="row dmbs-content">
+
+    <?php //left sidebar ?>
+    <?php get_sidebar( 'left' ); ?>
+
+    <div class="col-md-<?php devdmbootstrap3_main_content_width(); ?> dmbs-main">
+
+        <?php // theloop
+        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <?php the_content(); ?>
+            <?php wp_link_pages(); ?>
+            <?php comments_template(); ?>
+
+        <?php endwhile; ?>
+        <?php else: ?>
+
+            <?php get_404_template(); ?>
+
+        <?php endif; ?>
+
+    </div>
+
+</div>
+<!-- end content container -->
+
+<?php get_footer(); ?>
