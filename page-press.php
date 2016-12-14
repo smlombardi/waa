8db83008f2ba7d717b3186134ef1356e0694ab87
Steve Lombardi <smlombardi@gmail.com>
2016-12-14 12:43:07 -0500

    press page

--- page-press.php
+++ page-press.php
@@ -22,56 +22,69 @@ get_header(); ?>
         <?php // theloop
         if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="row">
-
-
-          <?php
-				    $args = array( 'post_type' => 'news_item', 'posts_per_page' => 2, 'orderby' => 'date', 'order' => 'DESC' );
-				    $loop = new WP_Query( $args );
-            $post_idx = 0;
-				    while ( $loop->have_posts() ) : $loop->the_post(); $post_idx++; ?>
-            <div class="col-md-4 col-<?php echo $post_idx; ?>">
-              <h3 class="press-head">
-                In the News
-              </h3>
-
-              <div class="news-item-block">
-                <?php 
-                  $newsImage = get_field('lede_image');
-                ?>
-                <div class="news-image">
-                <img src="<?php echo $newsImage['url']; ?>" alt="<?php echo $newsImage['alt']; ?>" class="img-responsive" />
-                <div class="photo-credit"><?php echo $newsImage['caption']; ?></div>
+          <!-- in the news -->
+          <div class="col-md-8">
+              <div class="row">
+                <div class="col-md-6">
+                  <h3>In the News</h3>
                 </div>
-
-
-                <div class="news-source"><?php the_field('news_source'); ?></div>
-                <div class="news-date">
-                  <?php 
-                    $event_date = get_field('news_item_date');
-                    $fancy_date = date('F j, Y',strtotime($event_date));
-                    echo $fancy_date;
-                    ?>
-                </div>
-                <div class="news-content">
-                  <?php
-                    $newsContent = get_the_content();
-                    $newsTitle = get_the_title();
-                  ?>
-                
-                  <b><?php echo $newsTitle; ?>:</b> <?php echo $newsContent; ?>
-                
-                </div>
-
-                <div class="news-link"><a href="">Read More</a> &raquo;</div>
               </div>
-            </div>
-          <?php endwhile;  ?>  <!-- end the loop -->
+              <div class="row">
+                <?php
+                  $args = array( 'post_type' => 'news_item', 'posts_per_page' => 2, 'orderby' => 'date', 'order' => 'DESC' );
+                  $loop = new WP_Query( $args );
+                  $post_idx = 0;
+                  while ( $loop->have_posts() ) : $loop->the_post(); $post_idx++; ?>
+                  <div class="col-md-6">
+                    
+
+                    <div class="news-item-block">
+                      <?php 
+                        $newsImage = get_field('lede_image');
+                      ?>
+                      <div class="news-image">
+                      <img src="<?php echo $newsImage['url']; ?>" alt="<?php echo $newsImage['alt']; ?>" class="img-responsive" />
+                      <div class="photo-credit"><?php echo $newsImage['caption']; ?></div>
+                      </div>
+
+
+                      <div class="news-source"><?php the_field('news_source'); ?></div>
+                      <div class="news-date">
+                        <?php 
+                          $event_date = get_field('news_item_date');
+                          $fancy_date = date('F j, Y',strtotime($event_date));
+                          echo $fancy_date;
+                          ?>
+                      </div>
+                      <div class="news-content">
+                        <?php
+                          $newsContent = get_the_content();
+                          $newsTitle = get_the_title();
+                        ?>
+                      
+                        <b><?php echo $newsTitle; ?>:</b> <?php echo $newsContent; ?>
+                      
+                      </div>
+
+                      <div class="news-link"><a href="">Read More</a> &raquo;</div>
+                    </div>
+                  </div>
+                <?php endwhile;  ?>  <!-- end the loop -->
         
-        
-        <div class="col-md-4"></div>
-        <div class="col-md-4"></div>
             
         </div>
+
+
+          
+          </div>
+          <div class="col-md-4">
+          
+          
+          
+          </div>
+          
+        </div>
+        
 
         <?php endwhile; ?>
         <?php else: ?>
