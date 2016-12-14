672484a18e39f6906674b4b6b48a3ae0e215ad88
Steve Lombardi <smlombardi@gmail.com>
2016-12-14 12:58:13 -0500

    added page slug body class function

--- functions.php
+++ functions.php
@@ -63,3 +63,13 @@ function footer_widgets_init() {
 
 }
 add_action( 'widgets_init', 'footer_widgets_init' );
+
+//Page Slug Body Class
+function add_slug_body_class( $classes ) {
+	global $post;
+		if ( isset( $post ) ) {
+		$classes[] = $post->post_type . '-' . $post->post_name;
+		}
+	return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
