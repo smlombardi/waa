679f2a6aec26dfd2bed5e92301ddd97963912c47
Steve Lombardi <smlombardi@gmail.com>
2017-07-04 09:02:47 -0400

    menu button to hamburger icon
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

--- template-part-topnav.php
+++ template-part-topnav.php
@@ -8,16 +8,16 @@
     </div>
 
     <div class="row dmbs-top-menu">
-        
+
         <nav class="navbar" role="navigation">
             <div class="container">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-1-collapse">
                         <span class="sr-only"><?php _e('Toggle navigation','devdmbootstrap3'); ?></span>
-                        <!--<span class="icon-bar"></span>
                         <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>-->
-                        MENU
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+
                     </button>
                 </div>
 
